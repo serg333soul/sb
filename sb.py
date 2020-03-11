be33708eebccf655cb0a616695a040ad6c8538e3
sergey <serg333soul@gmail.com>
2020-03-11 19:43:45 +0200

    3com

--- sb.py
+++ sb.py
@@ -5,9 +5,18 @@ def create_table():
     f = [[i if j * i == 0 else SPACE for j in range(12)] for i in range(12)]
     f[0] = [L_S[i] for i in range(12)]
 
-    for l in f:
-        print(l)
+    return f
 
         
 
-print(create_table())
+board = create_table()
+
+def print_board(board):
+    '''Печатаем доску в консоли'''
+    for i in range(len(board) - 1):
+        for j in range(len(board[i])):
+            print('|'.rjust(2), str(board[i][j]).rjust(2), end=' ')
+        print('')
+        print('-' * 68)
+
+print_board(board)
